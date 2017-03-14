f32002eb15bb5de7db3e9d7d9caad11872f3e2e1
bearski <santosh.sewlal@gmail.com>
2017-03-14 12:31:38 +0200

    usinf dotenv to load environment variables

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
-// modules =================================================
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
+  // modules =================================================
+  var express        = require('express');
+  var app            = express();
+  var bodyParser     = require('body-parser');
+  var methodOverride = require('method-override');
+  var dotenv = require('dotenv');
 
-// configuration ===========================================
+  // configuration ===========================================
 
-// config files
-var port = process.env.PORT || 8080; // set our port
-// var db = require('./config/db');
+  // There's no need to check if .env exists, dotenv will check this
+  // for you. It will show a small warning which can be disabled when
+  // using this in production.
+  dotenv.load();
+
+  // config files
+  var port = process.env.PORT || 8080; // set our port
+  // var db = require('./config/db');
 
 
-// get all data/stuff of the body (POST) parameters
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+  // get all data/stuff of the body (POST) parameters
+  app.use(bodyParser.json()); // parse application/json
+  app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+  app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+  app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+  app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
-// routes ==================================================
-require('./app/routes')(app); // pass our application into our routes
+  // routes ==================================================
+  require('./app/routes')(app); // pass our application into our routes
 
-// start app ===============================================
-app.listen(port);
-console.log('Magic happens on port ' + port); 			// shoutout to the user
-exports = module.exports = app; 						// expose app
+  // start app ===============================================
+  app.listen(port);
+  console.log('Magic happens on port ' + port); 			// shoutout to the user
+  exports = module.exports = app; 						// expose app
